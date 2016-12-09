349f4fa372b5b8bc6fc64ea5c1ad2e94f3ec0a8d
Guy <guypham@hotmail.com>
2016-12-08 21:10:47 -0800

    changed the componets titles and added navbar under the main.js page

---
+++ app/components/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// Import sub-components
+import About from './components/About';
+import Partners from './components/Partners';
+import Press from './components/Press';
+import Contact from './components/Contact';
+
+class Main extends React.Component{
+
+	constructor(props){
+
+		super(props);
+
+ render() {
+
+     <div className="menu-active-state-example">
+              <Menu alignment={Alignments.RIGHT}>
+               <MenuItem isActive><a>About</a></MenuItem>
+               <MenuItem><a>Partners</a></MenuItem>
+               <MenuItem><a>Press</a></MenuItem>
+               <MenuItem><a>Contact</a></MenuItem>
+               <MenuItem><a>Login</a></MenuItem>
+             </Menu>
+      </div>
+
+ }
