bafcee12fbdc56e8402d93012f6f534e8657015e
Andrew Kil <andrewkil86@gmail.com>
2016-12-10 14:26:20 -0800

    adding boilerplate components

---
+++ app/components/About_Mission.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About_Mission = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    )
+  }
+})
+
+export default About_Mission
---
+++ app/components/About_Officers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About_Officers = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    )
+  }
+})
+
+export default About_Officers
