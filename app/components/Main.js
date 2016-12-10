2ab0e340be11aadc29cc7f664e1e1e7322c4727c
Andrew Kil <andrewkil86@gmail.com>
2016-12-10 11:30:34 -0800

    adding main nav bar

--- app/components/Main.js
+++ app/components/Main.js
@@ -8,44 +8,59 @@ import React from 'react'
 const Main = React.createClass({
   render () {
     return (
-      <div id='main-nav' className='navbar navbar-fixed-top'>
-        <div className='container'>
+      <div id="main-nav" className="navbar navbar-fixed-top">
+  			<div className="container">
 
-          <div className='navbar-header'>
+  				<div className="navbar-header">
 
-            <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='#site-nav'>
-              <span className='icon-bar' /><span className='icon-bar' /><span className='icon-bar' />
-            </button>
-            <a className='navbar-brand scrollto' href='#home'>
-              <span className='logo-small'><img src='assetswe/logo-small.png' alt='' /></span>
-              <span className='to-top'><i className='fa fa-arrow-up' /></span>
+  					<button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
+  						<span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span>
+  					</button>
 
-              INSURRECTION MEDIA
+  					<a className="navbar-brand scrollto" href="#home">
+  						<span className="logo-small"><img src="assets/logo-small.png" alt=""/></span>
+  						<span className="to-top"><i className="fa fa-arrow-up"></i></span>
 
-              <img className='site-logo' src='assets/logo.png' alt='logo' />
-            </a>
+  						Qubico
 
-          </div>
 
-          <div id='site-nav' className='navbar-collapse collapse'>
-            <ul className='nav navbar-nav navbar-right'>
-              <li className='home'>
-                <a href='#home' className='scrollto'>Home</a>
-              </li>
-              <li className='about'>
-                <a href='#about' className='scrollto'>About</a>
-              </li>
-              <li className='team'>
-                <a href='#team' className='scrollto'>Team</a>
-              </li>
-              <li className='partners'>
-                <a href='#news' className='scrollto'>Partners</a>
+  						{/* <img className="site-logo" src="assets/logo.png" alt="logo" /> */}
+  					</a>
+
+  				</div>
+          <div id="site-nav" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav navbar-right">
+              <li className="sr-only">
+                <a href="#home" className="scrollto">Home</a>
               </li>
               <li>
-                <a href='#contact' className='scrollto'>Contact</a>
+                <a href="#about" className="scrollto">About</a>
               </li>
-              <li className='blog'>
-                <a href='#blog'>Blog</a>
+              <li>
+                <a href="#services" className="scrollto">Services</a>
+              </li>
+              <li>
+                <a href="#team" className="scrollto">Team</a>
+              </li>
+              <li>
+                <a href="#portfolio" className="scrollto">Portfolio</a>
+              </li>
+              <li>
+                <a href="#pricing" className="scrollto">Prices</a>
+              </li>
+              <li className="">
+                <a href="#news" className="scrollto">News</a>
+              </li>
+              <li>
+                <a href="#contact" className="scrollto">Contact</a>
+              </li>
+              <li className="submenu ">
+                <a href="blog/blog.html">Blog</a>
+                <ul>
+                  <li><a href="blog/blog-ajax.html">Ajax Blog</a></li>
+                  <li><a href="blog/blog.html">Standard Blog</a></li>
+                  <li><a href="blog/following-the-train.html">Single Blog Post</a></li>
+                </ul>
               </li>
             </ul>
           </div>
