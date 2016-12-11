c0a921df0e4f1b1cea22cf2a29029ab151b207d8
andrewnaeve <andrewnaeve@gmail.com>
2016-12-11 15:49:45 -0800

    fixed

--- app/ClientApp.js
+++ app/ClientApp.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router'
-import app from './app'
+import App from './App'
 
-render(<BrowserRouter><app /></BrowserRouter>,
+render(<BrowserRouter><App /></BrowserRouter>,
   document.getElementById('app')
 )
