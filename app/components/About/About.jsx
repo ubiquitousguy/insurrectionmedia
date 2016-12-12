bcf86ccdf655fa352437d0198f08d670ba3f2f33
Drake Smith <drakesmithjr@gmail.com>
2016-12-12 00:01:21 -0800

    about layout

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -2,12 +2,32 @@ import React from 'react'
 
 const About = React.createClass({
   render () {
-    const containerPlacer = {
-      background: "pink"
-    }
+
     return (
-      <div className="row" style={containerPlacer}>
-        <h1>About Main Component</h1>
+      <div className="container" id="about">
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <h1 className="text-sm-center">Insurrection Media</h1>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <img className="center-block img-responsive" src="http://lorempixel.com/250/300/people/" />
+          <h2 className="text-sm-center">Killaen</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus euismod ante, in gravida velit pharetra non. Suspendisse vitae ante in dolor pellentesque dignissim id et sem. Donec mattis interdum eleifend. Suspendisse maximus urna id efficitur facilisis. Morbi non nibh non odio dictum venenatis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi tincidunt accumsan odio sed tincidunt. Integer egestas ipsum nisl, sit amet vehicula justo dapibus id. Aenean sollicitudin dui a nulla vehicula, vitae bibendum augue dictum. Aenean quis eleifend turpis. Etiam vel quam pulvinar, sollicitudin neque ut, interdum tortor.
+          </p>
+        </div>
+        <div className="col-sm-4">
+          <img className="center-block img-responsive" src="http://lorempixel.com/250/300/people/" />
+          <h2 className="text-sm-center">Carmen</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus euismod ante, in gravida velit pharetra non. Suspendisse vitae ante in dolor pellentesque dignissim id et sem. Donec mattis interdum eleifend. Suspendisse maximus urna id efficitur facilisis. Morbi non nibh non odio dictum venenatis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi tincidunt accumsan odio sed tincidunt. Integer egestas ipsum nisl, sit amet vehicula justo dapibus id. Aenean sollicitudin dui a nulla vehicula, vitae bibendum augue dictum. Aenean quis eleifend turpis. Etiam vel quam pulvinar, sollicitudin neque ut, interdum tortor.
+          </p>
+        </div>
+        <div className="col-sm-4">
+          <img className="center-block img-responsive" src="http://lorempixel.com/250/300/people/" />
+          <h2 className="text-sm-center">Joe</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus euismod ante, in gravida velit pharetra non. Suspendisse vitae ante in dolor pellentesque dignissim id et sem. Donec mattis interdum eleifend. Suspendisse maximus urna id efficitur facilisis. Morbi non nibh non odio dictum venenatis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi tincidunt accumsan odio sed tincidunt. Integer egestas ipsum nisl, sit amet vehicula justo dapibus id. Aenean sollicitudin dui a nulla vehicula, vitae bibendum augue dictum. Aenean quis eleifend turpis. Etiam vel quam pulvinar, sollicitudin neque ut, interdum tortor.
+          </p>
+        </div>
       </div>
     )
   }
