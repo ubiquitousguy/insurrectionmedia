8bb7d2133cc1b6ec091f04869279b957c41a75d9
Guy <guypham@hotmail.com>
2016-12-15 15:17:10 -0800

    added content for the board

---
+++ Partners_Finance.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Partners_Finance = React.createClass({
+  render () {
+    return (
+      <div>
+      <br />
+      <br />
+      <br />
+        <h1>Partners_Finance Component</h1>
+      </div>
+    )
+  }
+})
+
+export default Partners_Finance
