c969e7304083c26f02cdde11f667170861d016ab
Guy <guypham@hotmail.com>
2016-12-15 20:39:52 -0800

    added css to the contact form

--- app/components/Contact/Contact.js
+++ app/components/Contact/Contact.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Contact_Gen from './Contact_Gen';
-import Contact_Media from './Contact_Media';
+// import Contact_Media from './Contact_Media';
 
 class Contact extends React.Component{
   render() {
     return (
-      <div>
+      <div className = "container-fluid">
         <Contact_Gen />
-        <Contact_Media />
       </div>
     )
   }
