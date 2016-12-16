1c78d6a794227aa696921ad197a4276006e0a6d2
Andrew Kil <andrewkil86@gmail.com>
2016-12-15 19:20:38 -0800

    navbar fix

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -6,7 +6,7 @@ const Nav = React.createClass({
 render () {
   return (
 <div className="container">
-  <nav className="navbar navbar-fixed-top navbar-dark bg-inverse">
+  <nav className="navbar navbar-fixed-top" id="navBar">
     <button className="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
     <div className="collapse navbar-toggleable-md" id="navbarResponsive">
       <Link to='/' className="navbar-brand">Insurrection Media</Link>
