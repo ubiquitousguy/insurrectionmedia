eccb5132e8bf2f78defddd17feb37865e56cbd31
Guy <guypham@hotmail.com>
2016-12-15 18:47:17 -0800

    added an id to the mission page

--- app/components/About/About_Mission.js
+++ app/components/About/About_Mission.js
@@ -3,9 +3,11 @@ import React from 'react'
 const About_Mission = React.createClass({
   render () {
     return (
-      <div>
-        <h1>Insurrection Media is an independent talent friendly digital TV studio developing and producing high quality scripted shows for over-the-top video platforms as well as broadcast and cable networks internationally. The company is headquartered in Los Angeles.
-        </h1>
+      <div id="about">
+        <div>
+          <h1>Insurrection Media is an independent talent friendly digital TV studio developing and producing high quality scripted shows for over-the-top video platforms as well as broadcast and cable networks internationally. The company is headquartered in Los Angeles.
+          </h1>
+        </div>
       </div>
     )
   }
