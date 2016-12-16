629bffbb793d6bdbe9411ebc47dcf23a92960897
andrewnaeve <andrewnaeve@gmail.com>
2016-12-16 15:50:33 -0800

    express session

--- server.js
+++ server.js
@@ -10,18 +10,26 @@ const fs = require('fs')
 const port = 3000
 const baseTemplate = fs.readFileSync('./index.html')
 const template = _.template(baseTemplate)
-
 const app = require('./app/app.js').default
-const cookieParser = require('cookieParser')
-
-const router = require('./router')
 
 const User = require('./app/models/Users')
+
+const cookieParser = require('cookieParser')
+
+
+
+
+const passport = require("passport")
+const session = require('express-session')
+
+const router = require('./router')
 
 const server = express()
 // class components
 const bodyParser = require('body-parser')
 const logger = require('morgan')
+
+
 // Passport Setup
 //const passport = require("passport");
 //const LocalStrategy = require('passport-local').Strategy;
@@ -44,25 +52,15 @@ server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.text())
 server.use(bodyParser.json({type: 'application/vnd.api+json'}))
-// server.use(express.cookieParser());
 
-// server.use(express.bodyParser());
-// server.use(express.session({ secret: 'SECRET' }));
-//server.use(passport.initialize());
-// server.use(passport.session());
+server.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+server.use(passport.initialize())
+server.use(passport.session())
+
+
+
 // server side rendering
-
 router(server)
-
-
-
-
-
-
-
-
-
-
 
 
 
