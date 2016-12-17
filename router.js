cc8433b6408e0055b8377f055bc4ed6ef6a5f7cb
Ceejay Varias <ceejayvarias@ucla.edu>
2016-12-16 17:54:37 -0800

    router

--- router.js
+++ router.js
@@ -2,10 +2,10 @@ const Promise = require('bluebird')
 const express = require('express')
 const User = require('./app/models/Users')
 
-const jwt = require('jsonwebtoken'),
-      crypto = require('crypto'),
-      config = require('./config/secret')
-const compareSec = require('./encryption').compareSec
+// const jwt = require('jsonwebtoken'),
+      // crypto = require('crypto'),
+      // config = require('./config/secret')
+// const compareSec = require('./encryption').compareSec
 
 
 function generateToken(user) {  
